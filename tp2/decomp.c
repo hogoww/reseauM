18b9c63b6f59ca067bf111d8fef0c8f5273e3dac
Hogoww <pierre_misse25@msn.com>
2017-10-16 19:07:28 +0200

    base decomp

---
+++ tp2/decomp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <errno.h>
+#include <string.h>
+
+void MainFunc();
+void ForkedFunc();
+
+int main(int argc,char** argv){
+  if(argc!=2){
+    fprintf(stderr,"\n\nUsage : ./decomp NB_PROCESSUS\n\n")
+  }
+  
+
+  if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+      || (errno != 0 && val == 0)) {
+    fprintf(stderr,"strtol failed : %s\");
+    exit(EXIT_FAILURE);
+  }
+  
+
+  for(size_t i=0;i<
+  
+}
